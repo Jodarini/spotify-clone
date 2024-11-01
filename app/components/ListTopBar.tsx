323c33a3d4355cfab4059fa5e81c281760b03d19
jodarini <rivasjose9428@gmail.com>
2024-11-01 16:55:17 -0500

    feat: add tooltip when hovering add or remove from library button
    style: other tooltip stylings

--- app/components/ListTopBar.tsx
+++ app/components/ListTopBar.tsx
@@ -15,6 +15,12 @@ import {
   saveAlbumsForCurrentUser,
 } from "../api/spotify/spotify-api";
 import { usePathname, useRouter } from "next/navigation";
+import {
+  TooltipProvider,
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+} from "@/components/ui/tooltip";
 
 export default function ListTopBar({
   playlistUri,
@@ -34,14 +40,14 @@ export default function ListTopBar({
 
   const addToLibrary = async () => {
     setInLibrary(true);
-    const contextId = playlistUri.split(":").splice(2);
+    const contextId = playlistUri!.split(":").splice(2);
     const res = await saveAlbumsForCurrentUser(token, contextId[0]);
     router.refresh();
   };
 
   const removeFromLibrary = async () => {
     setInLibrary(false);
-    const contextId = playlistUri.split(":").splice(2);
+    const contextId = playlistUri!.split(":").splice(2);
     const res = await removeAlbumforCurrentUser(token, contextId[0]);
     router.refresh();
   };
@@ -57,88 +63,104 @@ export default function ListTopBar({
       {isAlbum && (
         <>
           {inLibrary ? (
-            <button onClick={removeFromLibrary}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                stroke="currentColor"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="3"
-                viewBox="0 0 24 24"
-                data-icon="SvgCheck"
-                aria-hidden="true"
-                className="rounded-full p-1 bg-green text-black"
-              >
-                <path d="M3 14.7l2.7 2.45 2.7 2.45 6.3-7.6L21 4.4"></path>
-              </svg>{" "}
-            </button>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger onClick={removeFromLibrary}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    stroke="currentColor"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="3"
+                    viewBox="0 0 24 24"
+                    data-icon="SvgCheck"
+                    aria-hidden="true"
+                    className="rounded-full p-1 bg-green text-black"
+                  >
+                    <path d="M3 14.7l2.7 2.45 2.7 2.45 6.3-7.6L21 4.4"></path>
+                  </svg>{" "}
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Remove from Your Library</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
           ) : (
-            <button onClick={addToLibrary}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                stroke="currentColor"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                data-icon="SvgPlus"
-                aria-hidden="true"
-              >
-                <path d="M12 4v16m8-8H4"></path>
-              </svg>
-            </button>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger onClick={addToLibrary}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    stroke="currentColor"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    viewBox="0 0 24 24"
+                    data-icon="SvgPlus"
+                    aria-hidden="true"
+                  >
+                    <path d="M12 4v16m8-8H4"></path>
+                  </svg>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Add to Your Library</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
           )}
+
+          {/* <DropdownMenu> */}
+          {/*   <DropdownMenuTrigger> */}
+          {/*     <svg */}
+          {/*       xmlns="http://www.w3.org/2000/svg" */}
+          {/*       width="24" */}
+          {/*       height="24" */}
+          {/*       stroke="currentColor" */}
+          {/*       fill="none" */}
+          {/*       strokeLinecap="round" */}
+          {/*       strokeLinejoin="round" */}
+          {/*       strokeWidth="2" */}
+          {/*       viewBox="0 0 24 24" */}
+          {/*       data-icon="SvgMoreVertical" */}
+          {/*       aria-hidden="true" */}
+          {/*     > */}
+          {/*       <path d="M11 20a1 1 0 11.293.707A1 1 0 0111 20zm0-8a1 1 0 11.293.707A1 1 0 0111 12zm0-8a1 1 0 11.293.707A1 1 0 0111 4z"></path> */}
+          {/*     </svg> */}
+          {/*   </DropdownMenuTrigger> */}
+          {/*   <DropdownMenuContent> */}
+          {/*     {isInLibrary[0] ? ( */}
+          {/*       <DropdownMenuItem onClick={removeFromLibrary}> */}
+          {/*         Remove from Your Library */}
+          {/*       </DropdownMenuItem> */}
+          {/*     ) : ( */}
+          {/*       <DropdownMenuItem onClick={addToLibrary}> */}
+          {/*         Add to Your Library */}
+          {/*       </DropdownMenuItem> */}
+          {/*     )} */}
+          {/*     <DropdownMenuSeparator /> */}
+          {/*     <DropdownMenuItem>Add to profile</DropdownMenuItem> */}
+          {/*     <DropdownMenuSeparator /> */}
+          {/*     <DropdownMenuItem>Report</DropdownMenuItem> */}
+          {/*     <DropdownMenuItem> */}
+          {/*       Exclude from your taste profile */}
+          {/*     </DropdownMenuItem> */}
+          {/*     <DropdownMenuItem>Move to folder</DropdownMenuItem> */}
+          {/*     <DropdownMenuSeparator /> */}
+          {/*     <DropdownMenuItem>Share</DropdownMenuItem> */}
+          {/*     <DropdownMenuSeparator /> */}
+          {/*     <DropdownMenuItem>About recommendations</DropdownMenuItem> */}
+          {/*     <DropdownMenuSeparator /> */}
+          {/*     <DropdownMenuItem>Open in Desktop app</DropdownMenuItem> */}
+          {/*   </DropdownMenuContent> */}
+          {/* </DropdownMenu> */}
         </>
       )}
-      <DropdownMenu>
-        <DropdownMenuTrigger>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            stroke="currentColor"
-            fill="none"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            viewBox="0 0 24 24"
-            data-icon="SvgMoreVertical"
-            aria-hidden="true"
-          >
-            <path d="M11 20a1 1 0 11.293.707A1 1 0 0111 20zm0-8a1 1 0 11.293.707A1 1 0 0111 12zm0-8a1 1 0 11.293.707A1 1 0 0111 4z"></path>
-          </svg>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          {/* <DropdownMenuLabel>My Account</DropdownMenuLabel> */}
-          {isInLibrary[0] ? (
-            <DropdownMenuItem onClick={removeFromLibrary}>
-              Remove from Your Library
-            </DropdownMenuItem>
-          ) : (
-            <DropdownMenuItem onClick={addToLibrary}>
-              Add to Your Library
-            </DropdownMenuItem>
-          )}
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Add to profile</DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Report</DropdownMenuItem>
-          <DropdownMenuItem>Exclude from your taste profile</DropdownMenuItem>
-          <DropdownMenuItem>Move to folder</DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Share</DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>About recommendations</DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Open in Desktop app</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,76 +1,78 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-  		height: {
-  			'screen-minus-5rem': 'calc(100vh - 8rem)'
-  		},
-  		backgroundImage: {
-  			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-  			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
-  		},
-  		colors: {
-  			background: 'hsl(var(--background))',
-  			'background-alt': '#121211',
-  			'background-blue': '#1E3264',
-  			'background-purple': '#523BA1',
-  			tag: '#232323',
-  			'tag-hover': '#303031',
-  			green: '#1ED45E',
-  			'light-green': '#59e88b',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    extend: {
+      height: {
+        "screen-minus-5rem": "calc(100vh - 8rem)",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        background: "hsl(var(--background))",
+        "background-alt": "#121211",
+        "background-blue": "#1E3264",
+        "background-purple": "#523BA1",
+        "spotify-background": "#282828",
+        tag: "#232323",
+        "tag-hover": "#303031",
+        green: "#1ED45E",
+        "light-green": "#59e88b",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
